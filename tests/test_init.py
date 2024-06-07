4242ab2165510fe175fc53a5cbe54f33682c7061
Indu Prakash <6459774+iprak@users.noreply.github.com>
2024-06-07 06:03:57 -0500

    Currency symbol as unit (#135)
    * Added more symbols
    
    * Formatting changes
    
    * Addressed linter warnings
    
    Defining hass as HomeAssistant.
    Added missing None return type.
    Automatic formatting.
    
    * Changed SymbolDefinition to use constants
    
    * Added show_currency_symbol_as_unit
    
    * Updated readme

--- tests/test_init.py
+++ tests/test_init.py
@@ -13,6 +13,7 @@ from custom_components.yahoofinance.const import (
     CONF_INCLUDE_POST_VALUES,
     CONF_INCLUDE_PRE_VALUES,
     CONF_INCLUDE_TWO_HUNDRED_DAY_VALUES,
+    CONF_SHOW_CURRENCY_SYMBOL_AS_UNIT,
     CONF_SHOW_TRENDING_ICON,
     CONF_SYMBOLS,
     DEFAULT_CONF_DECIMAL_PLACES,
@@ -21,6 +22,7 @@ from custom_components.yahoofinance.const import (
     DEFAULT_CONF_INCLUDE_POST_VALUES,
     DEFAULT_CONF_INCLUDE_PRE_VALUES,
     DEFAULT_CONF_INCLUDE_TWO_HUNDRED_DAY_VALUES,
+    DEFAULT_CONF_SHOW_CURRENCY_SYMBOL_AS_UNIT,
     DEFAULT_CONF_SHOW_TRENDING_ICON,
     DEFAULT_SCAN_INTERVAL,
     DOMAIN,
@@ -31,8 +33,10 @@ from custom_components.yahoofinance.const import (
     SERVICE_REFRESH,
 )
 from homeassistant.const import CONF_SCAN_INTERVAL
+from homeassistant.core import HomeAssistant
 from homeassistant.setup import async_setup_component
-from tests import TEST_CRUMB, TEST_SYMBOL
+
+from . import TEST_CRUMB, TEST_SYMBOL
 
 SAMPLE_CONFIG = {DOMAIN: {CONF_SYMBOLS: [TEST_SYMBOL]}}
 YSUC = "custom_components.yahoofinance.YahooSymbolUpdateCoordinator"
@@ -45,33 +49,27 @@ DEFAULT_OPTIONAL_CONFIG = {
     CONF_INCLUDE_TWO_HUNDRED_DAY_VALUES: DEFAULT_CONF_INCLUDE_TWO_HUNDRED_DAY_VALUES,
     CONF_SCAN_INTERVAL: DEFAULT_SCAN_INTERVAL,
     CONF_SHOW_TRENDING_ICON: DEFAULT_CONF_SHOW_TRENDING_ICON,
+    CONF_SHOW_CURRENCY_SYMBOL_AS_UNIT: DEFAULT_CONF_SHOW_CURRENCY_SYMBOL_AS_UNIT,
     CONF_INCLUDE_FIFTY_TWO_WEEK_VALUES: DEFAULT_CONF_INCLUDE_FIFTY_TWO_WEEK_VALUES,
 }
+
 
 @pytest.mark.parametrize(
     "domain_config",
     [
         # a is invalid interval
-        {
-            CONF_SYMBOLS: ["xyz"],
-            CONF_SCAN_INTERVAL: "a"
-        },
+        {CONF_SYMBOLS: ["xyz"], CONF_SCAN_INTERVAL: "a"},
         # less than MINIMUM_SCAN_INTERVAL
         {
             CONF_SYMBOLS: ["xyz"],
-            CONF_SCAN_INTERVAL: MINIMUM_SCAN_INTERVAL + timedelta(seconds = -1)
+            CONF_SCAN_INTERVAL: MINIMUM_SCAN_INTERVAL + timedelta(seconds=-1),
         },
-        {
-            CONF_SYMBOLS: ["xyz"],
-            CONF_SCAN_INTERVAL: None
-        }
-    ]
+        {CONF_SYMBOLS: ["xyz"], CONF_SCAN_INTERVAL: None},
+    ],
 )
 async def test_invalid_global_scan_interval(
-    hass,
-    domain_config,
-    enable_custom_integrations
-):
+    hass: HomeAssistant, domain_config, enable_custom_integrations: None
+) -> None:
     """Check for invalid scan_interval."""
     assert await async_setup_component(hass, DOMAIN, {DOMAIN: domain_config}) is False
 
@@ -82,12 +80,20 @@ async def test_invalid_global_scan_interval(
         (
             # Simple format - no scan_interval defaults to DEFAULT_SCAN_INTERVAL
             {CONF_SYMBOLS: ["xyz"]},
-            {CONF_SYMBOLS: [SymbolDefinition("XYZ", scan_interval=DEFAULT_SCAN_INTERVAL)]},
+            {
+                CONF_SYMBOLS: [
+                    SymbolDefinition("XYZ", scan_interval=DEFAULT_SCAN_INTERVAL)
+                ]
+            },
         ),
         (
             # Expanded format - no scan_interval defaults to DEFAULT_SCAN_INTERVAL
             {CONF_SYMBOLS: [{"symbol": "xyz"}]},
-            {CONF_SYMBOLS: [SymbolDefinition("XYZ", scan_interval=DEFAULT_SCAN_INTERVAL)]},
+            {
+                CONF_SYMBOLS: [
+                    SymbolDefinition("XYZ", scan_interval=DEFAULT_SCAN_INTERVAL)
+                ]
+            },
         ),
         (
             # Mix of Simple and expanded formats - no scan_interval defaults to DEFAULT_SCAN_INTERVAL
@@ -102,12 +108,20 @@ async def test_invalid_global_scan_interval(
         (
             # Simple format - Duplicate removed, scan_interval defaults to DEFAULT_SCAN_INTERVAL
             {CONF_SYMBOLS: ["xyz", "xyz"]},
-            {CONF_SYMBOLS: [SymbolDefinition("XYZ", scan_interval=DEFAULT_SCAN_INTERVAL)]},
+            {
+                CONF_SYMBOLS: [
+                    SymbolDefinition("XYZ", scan_interval=DEFAULT_SCAN_INTERVAL)
+                ]
+            },
         ),
         (
             # Mixed formats - duplicate removed, scan_interval defaults to DEFAULT_SCAN_INTERVAL
             {CONF_SYMBOLS: [{"symbol": "xyz"}, "xyz"]},
-            {CONF_SYMBOLS: [SymbolDefinition("XYZ", scan_interval=DEFAULT_SCAN_INTERVAL)]},
+            {
+                CONF_SYMBOLS: [
+                    SymbolDefinition("XYZ", scan_interval=DEFAULT_SCAN_INTERVAL)
+                ]
+            },
         ),
         (
             # Simple format - custom global scan interval
@@ -146,7 +160,9 @@ async def test_invalid_global_scan_interval(
                 CONF_SCAN_INTERVAL: MANUAL_SCAN_INTERVAL,
             },
             {
-                CONF_SYMBOLS: [SymbolDefinition("XYZ", scan_interval=MANUAL_SCAN_INTERVAL)],
+                CONF_SYMBOLS: [
+                    SymbolDefinition("XYZ", scan_interval=MANUAL_SCAN_INTERVAL)
+                ],
                 CONF_SCAN_INTERVAL: MANUAL_SCAN_INTERVAL,
             },
         ),
@@ -157,29 +173,35 @@ async def test_invalid_global_scan_interval(
                 CONF_SCAN_INTERVAL: MANUAL_SCAN_INTERVAL,
             },
             {
-                CONF_SYMBOLS: [SymbolDefinition("XYZ", scan_interval=MANUAL_SCAN_INTERVAL)],
+                CONF_SYMBOLS: [
+                    SymbolDefinition("XYZ", scan_interval=MANUAL_SCAN_INTERVAL)
+                ],
                 CONF_SCAN_INTERVAL: MANUAL_SCAN_INTERVAL,
             },
         ),
         (
             # Expanded format - override None scan interval
             {
-                CONF_SYMBOLS: [{"symbol": "xyz", "scan_interval": MANUAL_SCAN_INTERVAL}],
-                CONF_SCAN_INTERVAL: 3600
+                CONF_SYMBOLS: [
+                    {"symbol": "xyz", "scan_interval": MANUAL_SCAN_INTERVAL}
+                ],
+                CONF_SCAN_INTERVAL: 3600,
             },
             {
-                CONF_SYMBOLS: [SymbolDefinition("XYZ", scan_interval=MANUAL_SCAN_INTERVAL)],
+                CONF_SYMBOLS: [
+                    SymbolDefinition("XYZ", scan_interval=MANUAL_SCAN_INTERVAL)
+                ],
                 CONF_SCAN_INTERVAL: timedelta(hours=1),
             },
         ),
     ],
 )
 async def test_scan_interval(
-    hass,
+    hass: HomeAssistant,
     domain_config,
     expected_partial_config,
-    enable_custom_integrations
-):
+    enable_custom_integrations: None,
+) -> None:
     """Component setup refreshed data coordinator and loads the platform."""
 
     mock_instance = MagicMock()
@@ -188,7 +210,9 @@ async def test_scan_interval(
     type(mock_instance).data = PropertyMock(return_value=None)
     type(mock_instance).last_update_success = PropertyMock(return_value=False)
 
-    with patch(YSUC, return_value=mock_instance), patch(f"{YCC}.try_get_crumb_cookies", AsyncMock(return_value=TEST_CRUMB)):
+    with patch(YSUC, return_value=mock_instance), patch(
+        f"{YCC}.try_get_crumb_cookies", AsyncMock(return_value=TEST_CRUMB)
+    ):
         config = {DOMAIN: domain_config}
 
         assert await async_setup_component(hass, DOMAIN, config) is True
@@ -203,8 +227,8 @@ async def test_scan_interval(
 
 
 async def test_setup_optionally_requests_coordinator_refresh(
-    hass, enable_custom_integrations
-):
+    hass: HomeAssistant, enable_custom_integrations: None
+) -> None:
     """Component setup requests data coordinator refresh if it failed to load data."""
 
     mock_instance = MagicMock()
@@ -213,7 +237,9 @@ async def test_setup_optionally_requests_coordinator_refresh(
     type(mock_instance).data = PropertyMock(return_value=None)
     type(mock_instance).last_update_success = PropertyMock(return_value=False)
 
-    with patch(YSUC, return_value=mock_instance), patch(f"{YCC}.try_get_crumb_cookies", AsyncMock(return_value=TEST_CRUMB)):
+    with patch(YSUC, return_value=mock_instance), patch(
+        f"{YCC}.try_get_crumb_cookies", AsyncMock(return_value=TEST_CRUMB)
+    ):
         assert await async_setup_component(hass, DOMAIN, SAMPLE_CONFIG) is True
         await hass.async_block_till_done()
 
@@ -222,12 +248,14 @@ async def test_setup_optionally_requests_coordinator_refresh(
 
 
 async def test_refresh_symbols_service(
-    hass,
-    enable_custom_integrations,
-):
+    hass: HomeAssistant,
+    enable_custom_integrations: None,
+) -> None:
     """Test refresh_symbols service callback."""
 
-    with patch(f"{YCC}.try_get_crumb_cookies", AsyncMock(return_value=TEST_CRUMB)), patch(
+    with patch(
+        f"{YCC}.try_get_crumb_cookies", AsyncMock(return_value=TEST_CRUMB)
+    ), patch(
         f"{YSUC}._async_update", AsyncMock(return_value=None)
     ) as mock_async_request_refresh:
         assert await async_setup_component(hass, DOMAIN, SAMPLE_CONFIG) is True
@@ -281,7 +309,7 @@ async def test_refresh_symbols_service(
 )
 def test_symbol_definition_comparison(
     sym1: SymbolDefinition, sym2: SymbolDefinition, expected: bool
-):
+) -> None:
     """Test SymbolDefinition instance comparison."""
 
     if expected:
@@ -303,6 +331,6 @@ def test_symbol_definition_comparison(
         ("164687040 0", None),
     ],
 )
-def test_convert_to_float(value, expected):
+def test_convert_to_float(value, expected) -> None:
     """Tests float conversion."""
     assert convert_to_float(value) == expected
