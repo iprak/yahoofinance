87288540c67402137f275f2e4827b3bf49dc9e2c
IP <leolite1@gmail.com>
2025-05-02 05:37:58 -0500

    Update user agent

--- custom_components/yahoofinance/const.py
+++ custom_components/yahoofinance/const.py
@@ -154,7 +154,7 @@ REQUEST_HEADERS: Final = {
     "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
     "accept-encoding": "gzip,deflate,br,zstd",
     "accept-language": "en-US,en;q=0.9",
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36",
 }
 """ Headers for all other requests. """
 
